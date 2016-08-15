2151cf8792d6725d77fa11ccdab0148eae8d7088
Phuong Dinh Van <Phuong Dinh Van>
2016-08-16 01:27:39 +0700

    modified

--- tests/testthat/test_hello.R
+++ tests/testthat/test_hello.R
@@ -6,3 +6,10 @@ test_that("Call hello function", {
   roots <- hello(str)
   expect_match( roots, str)
 })
+
+test_that("Percentage Value", {
+  x <- 0.56733
+  y <- 2
+  p <- percent(x, y)
+  expect_equal(p, "56.73%")
+})
