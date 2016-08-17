1400bbdc5229440bceab2d0a0fbb4103e6e9b370
Phuong Dinh Van <Phuong Dinh Van>
2016-08-17 20:44:30 +0700

    test uat

--- tests/testthat/test_hello.R
+++ tests/testthat/test_hello.R
@@ -7,9 +7,17 @@ test_that("Call hello function", {
   expect_match( roots, str)
 })
 
-test_that("Percentage Value", {
+test_that("Percentage Value 1", {
   x <- 0.56733
   y <- 2
   p <- percent(x, y)
   expect_equal(p, "56.73%")
 })
+
+test_that("Percentage Value 2", {
+  x <- 1.67332
+  y <- 4
+  p <- percent(x, y)
+  expect_equal(p, "167.332%")
+})
+
